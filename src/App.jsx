dcb79095bfec18c1874a2c97e6a41bf051e8dded
jakaria455173 <jakaria455173@gmail.com>
2023-04-30 04:01:24 +0600

    react tailwind starter templates

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div className='text-center font-bold text-blue-400 text-4xl mt-5'>This Is Our Home Components 😃</div>
+    </>
+  )
+}
+
+export default App
