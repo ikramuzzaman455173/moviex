685421314bf52f82a8706ce611b19fe3631e5778
ikramuzzaman455173 <jakaria455173@gmail.com>
2023-08-25 02:35:21 +0600

    api setup or call successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
-
+import {useState,useEffect} from 'react'
+import { fetchDataFormApi } from './api'
 const App = () => {
+  useEffect(() => {
+      apiTesting()
+    },[])
+  const apiTesting = () => {
+    fetchDataFormApi("/movie/popular").then(result => {
+            console.log(result);
+          }).catch(error => {
+            console.log(`Error:`,error.message);
+          })
+  }
   return (
     <>
       <div className='text-center font-bold text-blue-400 text-4xl mt-5'>This Is Our Home Components 😃</div>
