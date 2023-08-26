8fe29808ae5c4e7d54822adfe3f83d8983fd4b64
ikramuzzaman455173 <jakaria455173@gmail.com>
2023-08-27 03:22:12 +0600

    hero banner part completes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import { BrowserRouter,Route,Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { fetchDataFormApi } from './api'
 import { useSelector, useDispatch } from 'react-redux'
 import { getApiConfiguration } from './Store/HomeSlice';
@@ -13,28 +13,33 @@ import Header from './Components/Header/Header';
 const App = () => {
   const dispatch = useDispatch()
   const { url } = useSelector(state => state.home)
-  console.log({ url:url?.total_pages});
+  console.log({ url: url?.total_pages });
   useEffect(() => {
-      apiTesting()
-    },[])
-  const apiTesting = () => {
-    fetchDataFormApi("/movie/popular").then(result => {
+    fetchApiConfig()
+  }, [])
+  const fetchApiConfig = () => {
+    fetchDataFormApi("/configuration").then(result => {
       console.log(result);
-      dispatch(getApiConfiguration(result))
-          }).catch(error => {
-            console.log(`Error:`,error.message);
-          })
+      const url = {
+        backdrop: result.images.secure_base_url + "original",
+        poster: result.images.secure_base_url + "original",
+        profile: result.images.secure_base_url + "original"
+      }
+      dispatch(getApiConfiguration(url))
+    }).catch(error => {
+      console.log(`Error:`, error.message);
+    })
   }
   return (
     <>
       <BrowserRouter>
         {/* <Header/> */}
         <Routes>
-          <Route path="/" element={<HomePage/>} />
-          <Route path="/:mediaType/:id" element={<Details/>} />
-          <Route path="/search/:query" element={<SearchResult/>} />
-          <Route path="/expore/:mediaType" element={<Explore/>} />
-          <Route path="*" element={<ErrorPage/>} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/:mediaType/:id" element={<Details />} />
+          <Route path="/search/:query" element={<SearchResult />} />
+          <Route path="/expore/:mediaType" element={<Explore />} />
+          <Route path="*" element={<ErrorPage />} />
         </Routes>
         {/* <Footer/> */}
       </BrowserRouter>
--- src/Pages/HomePage/HeroBanner/HeroBanner.jsx
+++ src/Pages/HomePage/HeroBanner/HeroBanner.jsx
@@ -1,9 +1,22 @@
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 import { useNavigate } from "react-router-dom";
+import useFetch from '../../../Hooks/UseFetch';
+import { useSelector } from 'react-redux';
+import Img from '../../../Components/LazyLoadImage/LazyLoadImage';
+import ContentWrapper from '../../../Components/ContentWrapper/ContentWrapper';
 const HeroBanner = () => {
   const navigate = useNavigate()
   const [background, setBackground] = useState("")
   const [query, setQuery] = useState("")
+  const { data, loading } = useFetch("/movie/upcoming")
+  const {url} = useSelector(state=>state.home)
+  console.log({ data });
+  {/* ====random background generate functionality add===== */ }
+  useEffect(() => {
+    const bg = url.backdrop+data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path
+    console.log({ bg });
+    setBackground(bg)
+    },[data])
   const searchQueryHandler = (event) => {
     if (event.key === 'Enter' && query.length > 0) {
       navigate(`/search/${query}`)
@@ -11,7 +24,11 @@ const HeroBanner = () => {
   }
   return (
     <div className="heroBanner">
-      <div className="wrapper">
+    {!loading&&<div className="backdrop-img">
+        <Img src={background}/>
+      </div>}
+      <div className="opacity-layer"></div>
+      <ContentWrapper>
         <div className="heroBannerContent">
           <span className="title">Welcome.</span>
           <span className="subTitle">Millions of movies, TV shows and people to discover. Explore now.</span>
@@ -20,11 +37,10 @@ const HeroBanner = () => {
             <button>Search</button>
           </div>
         </div>
-      </div>
+      </ContentWrapper>
     </div>
   )
 }
 
 export default HeroBanner
-
 
