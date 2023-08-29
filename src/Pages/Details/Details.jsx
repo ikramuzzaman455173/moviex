ac2a56778751cd13292ff946d76076697e753cc0
ikramuzzaman455173 <jakaria455173@gmail.com>
2023-08-30 02:12:01 +0600

    DetailsBanner Part Completes

--- src/Pages/Details/Details.jsx
+++ src/Pages/Details/Details.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import useFetch from '../../Hooks/UseFetch'
+import { useParams } from 'react-router-dom'
+import DetailsBanner from './DetailsBanner/DetailsBanner'
 
-const Details= () => {
+const Details = () => {
+  const {mediaType,id} = useParams()
+  const {data,loading} = useFetch(`/${mediaType}/${id}/videos`)
+  const {data:credits,loading:creditsLoading} = useFetch(`/${mediaType}/${id}/credits`)
+
   return (
     <>
-      <div>This is our Details components</div>
+      <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
     </>
   )
 }
 
 export default Details
-
