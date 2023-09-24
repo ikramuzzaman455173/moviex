dd19d425560a38725368e824f37326181a407cd3
ikramuzzaman455173 <jakaria455173@gmail.com>
2023-09-25 02:28:33 +0600

    new think add

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,3 +1,111 @@
+// import React, { useState, useEffect } from "react";
+// import { HiOutlineSearch } from "react-icons/hi";
+// import { SlMenu } from "react-icons/sl";
+// import { VscChromeClose } from "react-icons/vsc";
+// import { useNavigate, useLocation, Link } from "react-router-dom";
+// import ContentWrapper from "../ContentWrapper/ContentWrapper";
+// import logo from "../../../src/assets/movix-logo.svg";
+
+// const Header = () => {
+//   const [show, setShow] = useState("top");
+//   const [lastScrollY, setLastScrollY] = useState(0);
+//   const [mobileMenu, setMobileMenu] = useState(false);
+//   const [query, setQuery] = useState("");
+//   const [showSearch, setShowSearch] = useState("");
+//   const navigate = useNavigate();
+//   const location = useLocation();
+//   useEffect(() => {
+//     window.scrollTo(0,0)
+//     },[location])
+//   const controlNavbar = () => {
+//     if (window.scrollY > 200) {
+//       if (window.scrollY > lastScrollY &&!mobileMenu) {
+//         setShow("hide")
+//       } else {
+//         setShow("show")
+//       }
+//     } else {
+//       setShow("top")
+//     }
+//     setLastScrollY(window.scrollY)
+//   }
+//   useEffect(() => {
+//     window.addEventListener("scroll", controlNavbar)
+//     return () => {
+//       window.removeEventListener("scroll",controlNavbar)
+//     }
+//     },[lastScrollY])
+
+
+
+//   const openSearch = () => {
+//     setMobileMenu(false)
+//     setShowSearch(true)
+//   }
+
+//   const openMobileMenu = () => {
+//     setMobileMenu(true)
+//     setShowSearch(false)
+//   }
+
+//   // const searchQueryHandler = (event) => {
+//   //   if (event.key == 'Enter' && query.length > 0) {
+//   //     navigate(`/search/${query}`)
+//   //   }
+//   //   setTimeout(() => {
+//   //     setShowSearch(false)
+//   //   }, 1000);
+//   // }
+
+//   const searchQueryHandler = (event) => {
+//     if (event.key === "Enter" && query.length > 0) {
+//         navigate(`/search/${query}`);
+//         setTimeout(() => {
+//             setShowSearch(false);
+//         }, 1000);
+//     }
+// };
+
+//   const navigationHandler = (type) => {
+//     if (type == 'movie') {
+//       navigate("/explore/movie")
+//     } else {
+//       navigate("/explore/tv")
+//     }
+//     setMobileMenu(false)
+//   }
+//   return (
+//     <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
+//       <ContentWrapper>
+//         <Link to="/" className="logo">
+//           <img src={logo} alt="logo image" />
+//         </Link>
+//         <ul className="menuItems">
+//           <li className="menuItem" onClick={()=>navigationHandler('movie')}>Movies</li>
+//           <li className="menuItem" onClick={()=>navigationHandler('tv')}>Tv Shows</li>
+//           <li className="menuItem"><HiOutlineSearch className="cursor-pointer" onClick={openSearch} /></li>
+//         </ul>
+//         <div className="mobileMenuItems">
+//           <HiOutlineSearch className="cursor-pointer" onClick={openSearch} />
+//           {mobileMenu ? <VscChromeClose onClick={() => setMobileMenu(false)} className="cursor-pointer" /> : <SlMenu className="cursor-pointer" onClick={openMobileMenu} />}
+//         </div>
+//       </ContentWrapper>
+//       {showSearch&&<div className="searchBar">
+//         <ContentWrapper>
+//           <div className="searchInput">
+//             <input type="text" onChange={(e) => setQuery(e.target.value)} onKeyUp={searchQueryHandler} placeholder="search for a movie and tv's show's...." />
+//             <VscChromeClose onClick={() => setShowSearch(false)} className="cursor-pointer" />
+//           </div>
+//         </ContentWrapper>
+//       </div>}
+//     </header>
+//   );
+// };
+
+// export default Header;
+
+
+
 import React, { useState, useEffect } from "react";
 import { HiOutlineSearch } from "react-icons/hi";
 import { SlMenu } from "react-icons/sl";
@@ -14,66 +122,61 @@ const Header = () => {
   const [showSearch, setShowSearch] = useState("");
   const navigate = useNavigate();
   const location = useLocation();
+
   useEffect(() => {
-    window.scrollTo(0,0)
-    },[location])
+    window.scrollTo(0, 0);
+  }, [location]);
+
   const controlNavbar = () => {
     if (window.scrollY > 200) {
-      if (window.scrollY > lastScrollY &&!mobileMenu) {
-        setShow("hide")
+      if (window.scrollY > lastScrollY && !mobileMenu) {
+        setShow("hide");
       } else {
-        setShow("show")
+        setShow("show");
       }
     } else {
-      setShow("top")
+      setShow("top");
     }
-    setLastScrollY(window.scrollY)
-  }
+    setLastScrollY(window.scrollY);
+  };
+
   useEffect(() => {
-    window.addEventListener("scroll", controlNavbar)
+    window.addEventListener("scroll", controlNavbar);
     return () => {
-      window.removeEventListener("scroll",controlNavbar)
-    }
-    },[lastScrollY])
-
-
+      window.removeEventListener("scroll", controlNavbar);
+    };
+  }, [lastScrollY]);
 
   const openSearch = () => {
-    setMobileMenu(false)
-    setShowSearch(true)
-  }
+    setMobileMenu(false);
+    setShowSearch(true);
+  };
 
   const openMobileMenu = () => {
-    setMobileMenu(true)
-    setShowSearch(false)
-  }
-
-  // const searchQueryHandler = (event) => {
-  //   if (event.key == 'Enter' && query.length > 0) {
-  //     navigate(`/search/${query}`)
-  //   }
-  //   setTimeout(() => {
-  //     setShowSearch(false)
-  //   }, 1000);
-  // }
+    setMobileMenu(true);
+    setShowSearch(false);
+  };
 
   const searchQueryHandler = (event) => {
     if (event.key === "Enter" && query.length > 0) {
+      // Debounce the search action by delaying it
+      console.log(event.target.value);
+      setTimeout(() => {
         navigate(`/search/${query}`);
-        setTimeout(() => {
-            setShowSearch(false);
-        }, 1000);
+        setShowSearch(false);
+      }, 1000);
     }
-};
+  };
 
   const navigationHandler = (type) => {
-    if (type == 'movie') {
-      navigate("/explore/movie")
+    if (type === "movie") {
+      navigate("/explore/movie");
     } else {
-      navigate("/explore/tv")
+      navigate("/explore/tv");
     }
-    setMobileMenu(false)
-  }
+    setMobileMenu(false);
+  };
+
   return (
     <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
       <ContentWrapper>
@@ -81,23 +184,55 @@ const Header = () => {
           <img src={logo} alt="logo image" />
         </Link>
         <ul className="menuItems">
-          <li className="menuItem" onClick={()=>navigationHandler('movie')}>Movies</li>
-          <li className="menuItem" onClick={()=>navigationHandler('tv')}>Tv Shows</li>
-          <li className="menuItem"><HiOutlineSearch className="cursor-pointer" onClick={openSearch} /></li>
+          <li className="menuItem" onClick={() => navigationHandler("movie")}>
+            Movies
+          </li>
+          <li className="menuItem" onClick={() => navigationHandler("tv")}>
+            Tv Shows
+          </li>
+          <li className="menuItem">
+            <HiOutlineSearch
+              className="cursor-pointer"
+              onClick={openSearch}
+            />
+          </li>
         </ul>
         <div className="mobileMenuItems">
-          <HiOutlineSearch className="cursor-pointer" onClick={openSearch} />
-          {mobileMenu ? <VscChromeClose onClick={() => setMobileMenu(false)} className="cursor-pointer" /> : <SlMenu className="cursor-pointer" onClick={openMobileMenu} />}
+          <HiOutlineSearch
+            className="cursor-pointer"
+            onClick={openSearch}
+          />
+          {mobileMenu ? (
+            <VscChromeClose
+              onClick={() => setMobileMenu(false)}
+              className="cursor-pointer"
+            />
+          ) : (
+            <SlMenu
+              className="cursor-pointer"
+              onClick={openMobileMenu}
+            />
+          )}
         </div>
       </ContentWrapper>
-      {showSearch&&<div className="searchBar">
-        <ContentWrapper>
-          <div className="searchInput">
-            <input type="text" onChange={(e) => setQuery(e.target.value)} onKeyUp={searchQueryHandler} placeholder="search for a movie and tv's show's...." />
-            <VscChromeClose onClick={() => setShowSearch(false)} className="cursor-pointer" />
-          </div>
-        </ContentWrapper>
-      </div>}
+      {showSearch && (
+        <div className="searchBar">
+          <ContentWrapper>
+            <div className="searchInput">
+              <input
+                type="text"
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyUp={searchQueryHandler}
+                placeholder="Search for a movie and TV show..."
+              />
+              <VscChromeClose
+                onClick={() => setShowSearch(false)}
+                className="cursor-pointer"
+              />
+            </div>
+          </ContentWrapper>
+        </div>
+      )}
     </header>
   );
 };
